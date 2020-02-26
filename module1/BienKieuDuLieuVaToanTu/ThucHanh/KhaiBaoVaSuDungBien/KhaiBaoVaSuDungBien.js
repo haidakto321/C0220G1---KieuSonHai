d50ed1fd37a53f62aea4b7e3a6f13f98f2ec2322
haidakto321 <kieusonhai07@gmail.com>
2020-02-26 10:35:45 +0700

    abc

---
+++ module1/BienKieuDuLieuVaToanTu/ThucHanh/KhaiBaoVaSuDungBien/KhaiBaoVaSuDungBien.js
@@ -0,0 +1,9 @@
+let f = 20.5;
+let b = true;
+let s = 'Ha Noi';
+document.write("i = " + i);
+document.write("br/>");
+document.write("f = " + f);
+document.write("br/>");
+document.write("b = " + b);
+document.write("s = " + s);
